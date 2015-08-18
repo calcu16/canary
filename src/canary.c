73b2af5e6afeacf3d9adeb0098d164e198b37e69
Andrew Carter <acarter@linkedin.com>
2015-08-17 20:41:22 -0700

    Adding in g6 parser and related structures

--- src/canary.c
+++ src/canary.c
@@ -1,5 +1,21 @@
 #define MAIN
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "g6.h"
+#include "graph.h"
 
-int main(int argc, char* argv[]) {
+int
+main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "%s GRAPH MINOR\n", argv[0]);
+    exit(1);
+  }
+  struct graph g, m;
+
+  atog(argv[1], &g);
+  atog(argv[2], &m);
+
+  printg(&g);
+  printg(&m);
   return 0;
 }
